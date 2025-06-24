e2c82f1b02395fc29c60c9014bdc7583494d0f60
Krzysztof Cywiński <k.m.cywinski@gmail.com>
2025-06-24 11:58:36 +0200

    Add Syno bridge SDK and test stub

---
+++ forge/test/SynoBridge.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import { BaseWormholeTunnelTest } from "@syno/testing/BaseWormholeTunnelTest.t.sol";
+
+contract SynoBridgeTest is BaseWormholeTunnelTest {
+
+    function test_SetUp() public {
+    }
+}
